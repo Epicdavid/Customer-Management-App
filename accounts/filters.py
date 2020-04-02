3eb209c9ad58f1109c3fd07530320b53ee974f0c
Epic david <toonerdrill@gmail.com>
2020-04-02 16:51:51 -0700

    Filter customer products

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,9 +1,10 @@
 import django_filters 
-
+from django_filters import DateFilter
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = 
     class Meta:
         model = Order
         fields = '__all__'
-        exclude = ['customer', 'date_ordered']
+        exclude = ['customer']
