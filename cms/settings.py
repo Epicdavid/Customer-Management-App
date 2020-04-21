a2defc5625bbca292e5d8e613c4964cd43c519af
Epic david <toonerdrill@gmail.com>
2020-04-20 17:33:10 -0700

    modified settings

--- cms/settings.py
+++ cms/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -159,3 +159,4 @@ AWS_S3_HOST = config('AWS_S3_HOST')
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME') 
 
 
+django_heroku.settings(locals())
