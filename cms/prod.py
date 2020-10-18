1ed7da5a524a1ea12b7785077effb4139763cc31
Epic david <toonerdrill@gmail.com>
2020-10-18 21:15:44 +0100

    prod fix

--- cms/prod.py
+++ cms/prod.py
@@ -1,8 +1,6 @@
 from .settings import *
 DEBUG = False
-import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
